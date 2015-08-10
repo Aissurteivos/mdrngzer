9ba40d736da01a56be48d4511792d186faca8e1c
Aissurteivos <traviz1232@gmail.com>
2015-08-09 21:08:33 -0400

    Added randomize button

--- src/rom.cpp
+++ src/rom.cpp
@@ -1,7 +1,7 @@
 #include "rom.h"
 
 ROM::ROM(std::string filepath) {
-    rom.open(filepath.c_str());
+    rom.open(filepath.c_str(), std::fstream::in | std::fstream::out | std::fstream::binary);
 }
 
 void ROM::randomizePokemon() {
